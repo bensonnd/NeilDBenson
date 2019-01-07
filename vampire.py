407441b8708dc8a5360f7bcf8932c543eed1e0b8
Neil D Benson <bensonnd@gmail.com>
2019-01-06 21:42:41 -0600

    Create vampire.py

---
+++ vampire.py
@@ -0,0 +1,10 @@
+if name == 'Alice':
+    print('Hi, ' + name + '.')
+elif age < 12:
+    print('You are not Alice, kiddo.')
+elif age > 2000:
+    print('Unlike you, Alice is not an undead, immortal vampire.')
+elif age > 100:
+    print('You are not Alice, granny.')
+else
+    print('You are are neither Alice nor a little kid')
