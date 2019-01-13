753497c31c98c11c81de0c3c9f02edb726b33292
Neil D Benson <bensonnd@gmail.com>
2019-01-13 15:14:58 -0600

    Update pivotFunction.py

--- pivotFunction.py
+++ pivotFunction.py
@@ -3,12 +3,12 @@ def listsPivot(pivotInput):
 degrees clockwise. ex. listToInvert[8,0] becomes listToInvert[0,0],
 listToInvert[0,0] becomes listToInvert[0,5] in 5x9 'grid'."""
     masterList = []
-    for i in range(len(grid[0])):	#012 - indeces of the first row
+    for i in range(len(grid[0])):	   #012 - indeces of the first row
         listHolder = []
-        for j in range(len(grid)):	#012345 - indeces of the first column
-            tempVar = grid[j][i] #invert the indeces
+        for j in range(len(grid)):	   #012345 - indeces of the first column
+            tempVar = grid[j][i]       #invert the indeces
             listHolder.append(tempVar) #updates the list with the inverted value. Creates a list for each row.
-        masterList.append(listHolder) #adds each new inverted row to the masterList
+        masterList.append(listHolder)  #adds each new inverted row to the masterList
     return masterList
 
 
